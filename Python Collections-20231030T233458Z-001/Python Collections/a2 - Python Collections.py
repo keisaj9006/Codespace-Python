e5337f9951f0b0a991b94c2ccee4caa30ffa077e
keisaj9006 <144917448+keisaj9006@users.noreply.github.com>
2023-10-31 12:23:35 +0000

    Add files via upload

---
+++ Python Collections-20231030T233458Z-001/Python Collections/a2 - Python Collections.py
@@ -0,0 +1,10 @@
+# Define the sample list
+sample_list = [20, 30, 25, 35, -16, 60, -100]
+
+# Calculate average
+list_sum = sum(sample_list)
+list_length = len(sample_list)
+average = list_sum / list_length
+
+# Print the result rounded to one decimal place
+print(f"Average value of the list elements is: {average:.1f}")

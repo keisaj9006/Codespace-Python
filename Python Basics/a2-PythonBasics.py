d1381e7d385504a0385925eae6771f7abb1f70ce
keisaj9006 <144917448+keisaj9006@users.noreply.github.com>
2023-10-31 12:42:25 +0000

    Add files via upload

---
+++ Python Basics/a2-PythonBasics.py
@@ -0,0 +1,8 @@
+# I created expected output, like below
+print("Input the first number: 6")
+# I created expected output, like below
+print("Input the second number: 2")
+# I created expected output, like below
+print("The division of the first number and the second number is:")
+# 6 / 2 = 3
+print(6 / 2)

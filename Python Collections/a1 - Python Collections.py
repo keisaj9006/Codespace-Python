3a2c1cbc12b9d69dfdd3fbe7a003825f6eca0810
keisaj9006 <144917448+keisaj9006@users.noreply.github.com>
2023-10-31 12:28:10 +0000

    Add files via upload

---
+++ Python Collections/a1 - Python Collections.py
@@ -0,0 +1,8 @@
+# Define the sample list
+sample_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Sum values from list
+list_sum = sum(sample_list)
+
+# Print the result
+print("The sum is", list_sum)
---
+++ Python Collections/a3 - Python Collections.py
@@ -0,0 +1,11 @@
+# Define the sample list
+sample_list = [25, 14, 56, 15, 36, 56, 77, 18, 29, 49]
+
+# Find the maximum and minimum value
+max_value = max(sample_list)
+min_value = min(sample_list)
+
+# Print the result
+print("Original List:", sample_list)
+print("Maximum value for the above list =", max_value)
+print("Minimum value for the above list =", min_value)
